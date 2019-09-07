50a07ff3e9ebea251e02cf6c03f8537d645216a9
BMasinde <masindeb@live.com>
2019-09-07 17:49:51 +0200

    changed birrds.rda to birds

--- tests/testthat/test_flysim.R
+++ tests/testthat/test_flysim.R
@@ -1,7 +1,7 @@
 
 context("data input in flysim")
 
-data("birds.rda")
+data("birds")
 
 vec_data <- c(5,4,7,8,9,10)
 
@@ -9,6 +9,7 @@ test_that("vector input is not allowed", {
   expect_error(flysim(vec_data))
 })
 
-test_that("wrong method throw error", {
+test_that("wrong method? throw error", {
   expect_error(flysim(data = birds, method = "bregt"))
 })
+
