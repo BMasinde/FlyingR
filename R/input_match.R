126e7e19c67ca1f4facca08dcfe3d8a08312176c
BMasinde <masindeb@live.com>
2019-09-24 04:06:38 +0200

    more check opitions

--- R/input_match.R
+++ R/input_match.R
@@ -5,9 +5,9 @@
   name <- grep("id|species.name|name|species name|species_name", names)
   wingSpan <- grep("ws|wing.span|wing_span|wing span|wingspan",
                    ignore.case = TRUE, names)
-  wingArea <- grep("wa|wing.area|wing area|wingarea",
+  wingArea <- grep("wa|wing.area|wing area|wingarea|wing_area",
                    ignore.case = TRUE,names)
-  ordo <- grep("order|ordo|order", ignore.case = TRUE, names)
+  ordo <- grep("order|ordo|ord", ignore.case = TRUE, names)
   bmass <- grep("body.mass|empty.mass|all-up_mass|allupmass|body_mass|bodymass",
                 ignore.case = TRUE, names)
   fmass <- grep("fat.mass|fatmass|fat_mass",
