4ec0f355b50b9548f235c54f67a28811b0c1d4f7
BMasinde <masindeb@live.com>
2019-11-06 06:59:42 +0100

    constant muscle mass
    time marching for cmm

--- R/input_match.R
+++ R/input_match.R
@@ -1,39 +1,65 @@
 
 # identify columns from data
 .colnames.match <- function(names) {
-  # name or id column
-  name <- grep("id|species.name|name|species name|species_name", names)
-  wingSpan <- grep("ws|wing.span|wing_span|wing span|wingspan",
+  id <- grepl("id", names)
+
+  name <- grepl("species.name|name|species name|species_name|scientific.name", names)
+
+  wingSpan <- grepl("ws|wing.span|wing_span|wing span|wingspan",
                    ignore.case = TRUE, names)
-  wingArea <- grep("wa|wing.area|wing area|wingarea|wing_area",
+
+  wingArea <- grepl("wa|wing.area|wing area|wingarea|wing_area",
                    ignore.case = TRUE,names)
-  ordo <- grep("order|ordo|ord|taxon", ignore.case = TRUE, names)
-  bmass <- grep("body.mass|empty.mass|all-up_mass|allupmass|body_mass|bodymass",
+
+  taxon <- grepl("order|ordo|ord|taxon", ignore.case = TRUE, names)
+
+  bMass <- grepl("body.mass|empty.mass|all-up_mass|allupmass|body_mass|bodymass",
                 ignore.case = TRUE, names)
-  fmass <- grep("fat.mass|fatmass|fat_mass",
+
+  fMass <- grepl("fat.mass|fatmass|fat_mass",
                 ignore.case = TRUE,names)
-  mmass <- grep("muscle.mass|musclemass|muscle_mass",
+
+  mMass <- grepl("muscle.mass|musclemass|muscle_mass",
                 ignore.case = TRUE,names)
 
-  names <- list("name" = name,
-                "bodyMass" = bmass,
-                "wingSpan" = wingSpan,
-                "fatMass" = fmass,
-                "order" = ordo,
-                "wingArea" =wingArea,
-                "muscleMass"  = mmass)
-
-  matches <- sapply(names, length)
-
-  # throw error mutliple matches
-  if(any(matches > 1) == TRUE) {
-    stop("One column per variable")
+  # only one with bird names
+  if (sum(name) > 2) {
+    stop("multiple name columns found. Remove one", call. = FALSE)
   }
 
-  if(any(matches == 0) == TRUE) {
-    mismatch <- which(matches == 0)
-    stop(paste(length(mismatch), "column(s) missing", sep = " "))
+  # missing column with id and name, set to false to gen
+  # if (sum(id) == 0 & sum(name) == 0) {
+  #   id == FALSE
+  # }
+
+  if (sum(taxon) == 0) {
+    stop("taxon column not found", call. = FALSE)
   }
 
-  return(names)
+  if (sum(bMass) == 0) {
+    stop("body mass not found", call. = FALSE)
+  }
+
+  if (sum(fMass) == 0) {
+    stop("fat mass not found", call. = FALSE)
+  }
+
+  if (sum(mMass) == 0) {
+    warning("muscle mass not found", call. = FALSE)
+  }
+
+
+
+
+  colIndex <- list("name" = which(name == TRUE),
+                "bodyMass" = which(bMass == TRUE),
+                "wingSpan" = which(wingSpan == TRUE),
+                "fatMass" = which(fMass == TRUE),
+                "order" = which(taxon == TRUE),
+                "wingArea" = which(wingArea == TRUE),
+                "muscleMass"  = which(mMass == TRUE),
+                "id" = which(id == TRUE)
+                )
+
+  return(colIndex)
 }
