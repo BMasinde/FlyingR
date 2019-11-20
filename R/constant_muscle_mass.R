7a810e559618f092fb90714b426b8e9aff0ecaf2
BMasinde <masindeb@live.com>
2019-11-20 20:21:55 +0100

    nested lists in simulation

--- R/constant_muscle_mass.R
+++ R/constant_muscle_mass.R
@@ -36,14 +36,14 @@
   # time marching
 
   simResults <- list(
-    bm = data.frame(row.names = id),
-    fm = data.frame(row.names = id),
-    dist = data.frame(row.names = id),
-    deltaM = data.frame(row.names = id),
-    E = data.frame(row.names = id),
-    minSpeed = data.frame(row.names = id),
-    trueSpeed = data.frame(row.names = id),
-    rvvmp = data.frame(row.names = id)
+    bm = rep(list(vector()), nrow(data)),
+    fm = rep(list(vector()), nrow(data)),
+    dist = rep(list(vector()), nrow(data)),
+    deltaM = rep(list(vector()), nrow(data)),
+    E = rep(list(vector()), nrow(data)),
+    minSpeed = rep(list(vector()), nrow(data)),
+    trueSpeed = rep(list(vector()), nrow(data)),
+    rvvmp = rep(list(vector()), nrow(data))
   )
 
   if (speed_control == "constant_speed") {
@@ -51,45 +51,45 @@
     for (i in 1:nrow(data)) {
       #i <- 1
       # initial values
-      simResults$bm[i, 1] <- allMass[i]
-      simResults$fm[i, 1] <- fatMass[i]
-      currentFM <- simResults$fm[i,1]
+      simResults$bm[[i]][1] <- allMass[i]
+      simResults$fm[[i]][1] <- fatMass[i]
+      currentFM <- simResults$fm[[i]][1]
 
       j <- 1
       while (currentFM > 0.00001) {
         # find speed and power
-        simResults$minSpeed[i, j] <-
-          .min.pow.speed(simResults$bm[i, j], wingSpan[i], cons)
+        simResults$minSpeed[[i]][j] <-
+          .min.pow.speed(simResults$bm[[i]][j], wingSpan[i], cons)
 
-        simResults$trueSpeed[i, j] <- simResults$minSpeed[i, j] * 1.2
+        simResults$trueSpeed[[i]][j] <- simResults$minSpeed[[i]][j] * 1.2
 
-        simResults$rvvmp[i, j] <-
-          simResults$trueSpeed[i, j] / simResults$minSpeed[i, j]
+        simResults$rvvmp[[i]][j] <-
+          simResults$trueSpeed[[i]][j] / simResults$minSpeed[[i]][j]
 
         # mechanical power
         power <-
-          .total.mech.power(simResults$bm[i, j],
+          .total.mech.power(simResults$bm[[i]][j],
                             wingSpan[i],
                             wingArea[i],
-                            simResults$trueSpeed[i, j],
+                            simResults$trueSpeed[[i]][j],
                             cons)
 
         # chemical power
         E <-
-          (power / cons$mce) + .basal.met2(cons, simResults$bm[i, j], simResults$fm[i, j], taxon[i])
+          (power / cons$mce) + .basal.met2(cons, simResults$bm[[i]][j], simResults$fm[[i]][j], taxon[i])
 
         #increase E by 10% to account for respiratory and heart
         E <- E + (E * 0.1)
 
-        simResults$E[i,j] <- E
+        simResults$E[[i]][j] <- E
 
         # update body
-        simResults$deltaM[i, j] <- (simResults$E[i, j] / cons$energy) * 360
-        simResults$fm[i, j + 1] <- simResults$fm[i, j] - simResults$deltaM[i, j]
-        simResults$bm[i, j + 1] <- simResults$bm[i, j] - simResults$deltaM[i, j]
+        simResults$deltaM[[i]][j] <- (simResults$E[[i]][j] / cons$eFat) * 360
+        simResults$fm[[i]][j + 1] <- simResults$fm[[i]][j] - simResults$deltaM[[i]][j]
+        simResults$bm[[i]][j + 1] <- simResults$bm[[i]][j] - simResults$deltaM[[i]][j]
 
-        currentFM <- simResults$fm[i, j] - simResults$deltaM[i, j]
-        simResults$dist[i, j] <- simResults$trueSpeed[i, j] * 360
+        currentFM <- simResults$fm[[i]][j] - simResults$deltaM[[i]][j]
+        simResults$dist[[i]][j] <- simResults$trueSpeed[[i]][j] * 360
         # increment counter
         j <- j + 1
       }
