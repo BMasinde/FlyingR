644d45b31670347d81943d1e6522bf3df4765c83
BMasinde <masindeb@live.com>
2019-11-20 10:23:36 +0100

    test path to data
    missing path in fileArgs throws error

--- tests/testthat/test_path_to_data.R
+++ tests/testthat/test_path_to_data.R
@@ -7,3 +7,7 @@ path <- "/Users/masinde/Documents/R projects/Flight project/Preset_birds.csv"
 test_that("Wrong argument in fileArgs", {
   expect_error(.pathToData(fileArgs = list(path, X1 = "random")))
 })
+
+test_that("Missing path throws error", {
+  expect_error(.pathToData(fileArgs = list(header = TRUE)))
+})
