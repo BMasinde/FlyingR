3855d8e6a513080695baead0c5b885341d157ae5
BMasinde <masindeb@live.com>
2019-12-11 23:48:42 +0100

    results as object
    migrate S3 class, results saving and naming

--- R/migrate.R
+++ R/migrate.R
@@ -7,18 +7,26 @@
 #' @name migrate
 #'
 #' @param data A data frame.
-#' @param fileArgs Arguments for path to data
-#' @param control A list for re-defining constants. See details.
+#' @param settings A list for re-defining constants. See details.
 #' @param method Methods for fuel management
 #' @param speed_control One of two speed control methods. By default
 #'        \textit{constant_speed} is used. \textit{vvmp_constant} is the alternative.
 #'        The former holds the true airspeed constant while the latter holds the
 #'        ratio of true airspeed and minimum power speed constant.
+#' @param file The name of the file which the data are to read from
+#' @param header Logical. If TRUE use first row as column headers
+#' @param sep separator
+#' @param quote The set of quoting characters. see read.csv
+#' @param dec The character used in the file for decimal points.
+#' @param fill See read.csv
+#' @param comment.char For more details see read.csv
+#' @param ... further arguments see read.csv
 #'
 #' @details The option *control takes the folowing arguments
 #' \itemize{
 #'    \item ppc: Profile power constant
 #'    \item eFat: Energy content of fuel from fat
+#'    \item eProtein: Energy content of protein
 #'    \item g: Accelaration due to gravity
 #'    \item mce: Mechanical conversion efficiency [0,1]
 #'    \item ipf: Induced power factor
@@ -28,47 +36,113 @@
 #'    \item alpha: Basal metabolism factors in passerines and non passerines
 #'    \item delta: Basal metabolism factors in passerines and non passerines
 #'    alpha*bodyMass^delta
+#'    \item invPower
+#'    \item speedRatio: True air speed to minimum power speed ratio
+#'    \item muscDensity: Density of the flight muscles.
+#'    \item phr: Protein hydration ratio
 #'}
-#' @include misc_functions.R lookup_table2.R input_match.R method_1.R method_2.R
+#' @include control.R misc_functions.R lookup_table2.R input_match.R
 #' @return S3 class object with range estimates based on methods defined and
-#'        constants
+#'        settings
 #' \itemize{
 #'    \item data as a data frame
-#'    \item range estimates
+#'    \item range estimates (Km)
 #'    \item fuel
-#'    \item constants (list)
+#'    \item settings (named vector)
 #' }
 #'
 #' @import Rcpp
 #' @export migrate
 #'
 #' @examples
-#' migrate(data = birds, ctrl = list(energy = 3.89*10^7))
-#' migrate(data = birds,  ctrl = list(airDensity = 0.905))
+#' migrate(data = birds, settings = list(energy = 3.89*10^7))
+#' migrate(data = birds,  method = "cmm", settings = list(airDensity = 0.905))
 #'
-#' @usage migrate(data, ctrl = list())
+#'
+#' @usage migrate(data, settings = list(), method = "cmm", speed_control = "constant_speed")
 #'
 
 
-migrate <- function(fileArgs = list(), data, control = list(), method = "cmm",
-                    speed_control = "constant_speed", ...) {
+migrate <- function(file, header = TRUE, sep = ",", quote = "\"", dec = ".",
+                    fill = TRUE, comment.char = "", ...,
+                    data, settings = list(), method = "cmm",
+                    speed_control = "constant_speed") {
 
-  if (missing(fileArgs) == TRUE & missing(data) == TRUE) {
+  # object with results
+  results <- list(
+    range = vector(),
+    #dist = data.frame(), not req (increment by same amount)
+    bodyMass = list(),
+    fatMass = list(),
+    muscleMass = list(),
+    mechPow = list(),
+    chemPow = list()
+  )
+
+  # both file and data not given throw an error
+  if (missing(file) == TRUE & missing(data) == TRUE) {
     stop("Data not found \n", call. = TRUE)
   }
 
-  if (missing(fileArgs) == FALSE & missing(data) == FALSE) {
+  if (missing(file) == FALSE & missing(data) == FALSE) {
     stop("Both path and data given. Function needs only one of the two \n", call. = TRUE)
   }
 
-  if(speed_control != "constant_speed" | speed_control != "vvmp_constant") {
-    stop("Wrong speed control  argument", call. = TRUE)
+  if(speed_control != "constant_speed" & speed_control != "vvmp_constant") {
+    stop("Wrong speed control  argument \n", call. = TRUE)
   }
 
-  if(missing(fileArgs) == FALSE) {
-    data <- .pathToData(fileArgs, ...)
+  # process data
+  if(missing(file) == FALSE) {
+    dataRead <- read.csv(file = file, header = header, sep = sep,quote = quote,
+                         dec = dec, fill = fill, comment.char, ...)
+    data <- .colnames.match(dataRead)
+  } else {
+    data <- .colnames.match(data)
   }
 
+  # control constants using default vs supplied
+  if(missing(settings) == TRUE) {
+    cons <- .control()
+  } else {
+    cons <- .control(settings)
+  }
+
+  if(method == "cmm") {
+    simulation <- .constant_muscle_mass(data, cons, speed_control)
+  } else {
+    print("No other method for now")
+  }
+
+  # aggregate dist from simulation to get range in Km
+  results$range <- sapply(simulation$dist, function(x) sum(x)/1000)
+
+  # range as named vectors
+  if (!is.null(data$name)) {
+    names(results$range) <- as.vector(data$name)
+    for(i in 1:length(simulation)) {
+      for(j in 1:length(simulation[[i]])){
+        names(simulation[[i]]) <- as.vector(data$name)
+      }
+    }
+  } else {
+    names(results$range) <- as.vector(data$ID)
+    for(i in 1:length(simulation)) {
+      for(j in 1:length(simulation[[i]])){
+        names(simulation[[i]]) <- as.vector(data$ID)
+      }
+    }
+  }
+
+  # store the rest of results
+  results$mechPow <- simulation$mechPow
+  results$bm <- simulation$bm
+  results$fm <- simulation$fm
+  results$chemPow <- simulation$E
+
+  # return object of class migrate
+  class(results) <- append(class(results), "migrate")
+  return(results)
 
 }
 
