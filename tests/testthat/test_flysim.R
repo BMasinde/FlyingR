5dfd07799eff8f4ea8dd81d8fe53e976c3bc8e37
BMasinde <masindeb@live.com>
2019-12-12 16:31:15 +0100

    updated tests
    tests updated arguments named appropriately

--- tests/testthat/test_flysim.R
+++ tests/testthat/test_flysim.R
@@ -9,19 +9,19 @@ test_that("vector input is not allowed", {
   expect_error(flysim(vec_data))
 })
 
-test_that("wrong method? throw error", {
-  expect_error(flysim(data = birds, method = "bregt"))
-})
+# test_that("wrong method? throw error", {
+#   expect_error(flysim(data = birds, method = "bregt"))
+# })
 
 data2 <- birds[, -3]
 test_that("less number of columns", {
   expect_error(flysim(data = data2))
 })
 
-test_that("Consumption", {
-  expect_error(flysim(data = birds, ctrl = list(consume = 10)))
-})
+# test_that("Consumption", {
+#   expect_error(flysim(data = birds, ctrl = list(consume = 10)))
+# })
 
 test_that("alpha input", {
-  expect_error(flysim(data = birds, ctrl = list(alpha = 1)))
+  expect_error(flysim(data = birds, settings = list(alpha = 1)))
 })
