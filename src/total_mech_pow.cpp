08f7c6d14ba6e9234ae45a9e886aeda0e130cfb6
BMasinde <masindeb@live.com>
2019-12-13 00:16:33 +0100

    doc not needed
    internal function no formal documentation needed

--- src/total_mech_pow.cpp
+++ src/total_mech_pow.cpp
@@ -1,13 +1,13 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-//' Function calculates total mechanical power
-//' @param bm body mass
-//' @param ws wing span
-//' @param wa wing area
-//' @param vt true speed
-//' @return totalMechPower total mechanical power
-//' @export
+// Function calculates total mechanical power
+// @param bm body mass
+// @param ws wing span
+// @param wa wing area
+// @param vt true speed
+// @return totalMechPower total mechanical power
+// @export
 
 // [[Rcpp::export(.total_Mech_Pow_cpp)]]
 NumericVector total_Mech_Pow_cpp(NumericVector bm, NumericVector ws, NumericVector wa,
