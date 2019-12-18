e66de253058175f01d96cacf8f48e5ff848d772e
BMasinde <masindeb@live.com>
2019-12-18 13:09:18 +0100

    muscle mass column
    Added muscle mass column using 0.17 muscle fraction

--- R/birds_documentation.R
+++ R/birds_documentation.R
@@ -9,12 +9,13 @@
 #' \describe{
 #'    \item{Scientific.name}{Name of bird species}
 #'    \item{Empty.mass}{Body mass in Kg. Includes fuel. All-up mass with crop
-#'    empty}
+#'    empty. Not to be confused with lean mass.}
 #'    \item{Wing.span}{Length of wings spread out in metres}
 #'    \item{Fat.mass}{Mass of fat that is consumable as fuel in Kg}
 #'    \item{Order}{Order of the spicies (passerine vs non-passerine)}
 #'    \item{Wing.area}{Area of both wing projected on a flat surface in metres
 #'    squared}
+#'    \item{Muscle.mass}{Mass in Kg. of flight muscles}
 #'
 #' }
 #'
