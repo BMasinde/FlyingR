ab22cbf12b857f90e68cf74512855353b7eaba3e
BMasinde <masindeb@live.com>
2020-07-08 19:21:42 +0200

    test on duration
    Trying to improve code coverage

--- tests/testthat/test_stopover_mass_calculator.R
+++ tests/testthat/test_stopover_mass_calculator.R
@@ -4,3 +4,10 @@ test_that("missing file and data throws error", {
   expect_error(stopover.mass.calculator(data = NULL
   ))
 })
+
+data <- data("birds")
+test_that("negaative duration throws error", {
+  expect_error(stopover.mass.calculator(data = data, duration = -2
+  ))
+})
+
