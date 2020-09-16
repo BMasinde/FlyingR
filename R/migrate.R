4024ab7d06c1767d4fccb0b0213501b0ec069aba
BMasinde <masindeb@live.com>
2020-09-16 15:30:54 +0200

    new return list/documentation

--- R/migrate.R
+++ R/migrate.R
@@ -20,29 +20,28 @@
 #'        \emph{1} is used. \emph{0} is the alternative.
 #'        The former holds the true airspeed constant while the latter holds the
 #'        ratio of true airspeed and minimum power speed constant.
-#' @param protein_met Percentage of energy attributed to protein and metabolism.
+#' @param protein_met Percentage of energy attributed to protein from metabolism.
 #'        Default value is 5 percent (0.05).
 #'
 #' @details The option *control takes the following arguments
 #' \itemize{
-#'    \item ppc: Profile power constant
-#'    \item fed: Energy content of fuel from fat
-#'    \item ped: Energy content of protein
-#'    \item g: Accelaration due to gravity
+#'    \item ppc: Profile power constant (8.4)
+#'    \item fed: Energy content of fuel from fat (3.9E+07)
+#'    \item ped: Energy content of protein (1.8E+07)
+#'    \item g: Accelaration due to gravity (9.81)
 #'    \item mce: Mechanical conversion efficiency [0,1]. Efficiency at which
-#'    mechanical power is converted to chemical power. Recommended value of 0.23.
-#'    \item ipf: Induced power factor
-#'    \item vcp: Ventilation and circulation power
-#'    \item airDensity: Air density at cruising altitude
-#'    \item bdc: Body drag coefficient
-#'    \item alpha: Basal metabolism factors in passerines and non passerines
-#'    \item delta: Basal metabolism factors in passerines and non passerines
+#'    mechanical power is converted to chemical power. (0.23)
+#'    \item ipf: Induced power factor (1.2)
+#'    \item vcp: Ventilation and circulation power (1.1)
+#'    \item airDensity: Air density at cruising altitude (1.00)
+#'    \item bdc: Body drag coefficient (0.1)
+#'    \item alpha: Basal metabolism factors in passerines and non passerines (6.25, 3.79)
+#'    \item delta: Basal metabolism factors in passerines and non passerines (0.724, 0.723)
 #'    alpha*bodyMass^delta.
-#'    \item mipd: Inverse power density of the mitochondria. Default value of
-#'    1.2E-06.
-#'    \item speedRatio: True air speed to minimum power speed ratio
-#'    \item muscDensity: Density of the flight muscles.
-#'    \item phr: Protein hydration ratio. Default value of 2.2
+#'    \item mipd: Inverse power density of the mitochondria. (1.2E-06)
+#'    \item speedRatio: True air speed to minimum power speed ratio (1.2)
+#'    \item muscDensity: Density of the flight muscles. (1060)
+#'    \item phr: Protein hydration ratio. (2.2)
 #'}
 #' @include control.R input_match.R misc_functions.R constant_muscle_mass.R
 #' @return S3 class object with range estimates based on methods defined and
@@ -74,17 +73,6 @@ migrate <- function(file, header = TRUE, sep = ",", quote = "\"", dec = ".",
                     data = NULL, settings = list(), method = "cmm",
                     speed_control = 1, protein_met = 0) {
 
-  # object with results
-  results <- list(
-    range = vector(),
-    #dist = data.frame(), not req (increment by same amount)
-    bodyMass = list(),
-    fatMass = list(),
-    muscleMass = list(),
-    mechPow = list(),
-    chemPow = list()
-  )
-
   # both file and data not given throw an error
   if (missing(file) == TRUE & is.null(data) == TRUE) {
     stop("Data not found \n", call. = TRUE)
@@ -103,7 +91,7 @@ migrate <- function(file, header = TRUE, sep = ",", quote = "\"", dec = ".",
     stop("min_protein within [0,1] \n", call. = TRUE)
   }
 
-  # process data
+  # process data ###############################################################
   if (missing(file) == FALSE) {
     dataRead <- read.csv(file = file, header = header, sep = sep, quote = quote,
                          dec = dec, fill = fill, comment.char,
@@ -112,6 +100,17 @@ migrate <- function(file, header = TRUE, sep = ",", quote = "\"", dec = ".",
   } else {
     data <- .colnames.match(data)
   }
+
+  n <- nrow(data)
+  # object with results ########################################################
+  results <- list(
+    range = vector(length = n),
+    bodyMass = vector(length = n),
+    fatMass = vector(length = n),
+    muscleMass = vector(length = n),
+    startMinSpeed = vector(length = n),
+    endMinSpeed = vector(length = n)
+  )
 
   # control constants using default vs supplied
   if (missing(settings) == TRUE) {
@@ -132,31 +131,28 @@ migrate <- function(file, header = TRUE, sep = ",", quote = "\"", dec = ".",
   }
 
   # aggregate dist from simulation to get range in Km
-  results$range <- sapply(simulation$dist, function(x) sum(x) / 1000)
-
+  results$range <- sapply(simulation$distance, function(x) sum(x) / 1000)
+  results$bodyMass <- simulation$allUpMass
+  results$fatMass <- round(simulation$fatMass,2)
+  results$muscleMass <- simulation$muscleMass
+  results$startMinSpeed <- simulation$startMinSpeed
+  results$endMinSpeed <- simulation$endMinSpeed
   # range as named vectors
   if (!is.null(data$name)) {
-    names(results$range) <- as.vector(data$name)
+    #names(results$range) <- as.vector(data$name)
     for (i in seq_len(length(simulation))) {
-        names(simulation[[i]]) <- as.vector(data$name)
+        names(results[[i]]) <- as.vector(data$name)
     }
   } else {
-    names(results$range) <- as.vector(data$ID)
+    #names(results$range) <- as.vector(data$ID)
     for (i in seq_len(length(simulation))) {
-      names(simulation[[i]]) <- as.vector(data$ID)
+      names(results[[i]]) <- as.vector(data$ID)
     }
   }
-
-  # store the rest of results
-  results$mechPow <- simulation$mechPow
-  results$bodyMass <- simulation$bm
-  results$fatMass <- simulation$fm
-  results$chemPow <- simulation$E
 
   # return object of class migrate
   class(results) <- append(class(results), "migrate")
   return(results)
-
 }
 
 
