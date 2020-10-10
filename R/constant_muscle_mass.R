e77380e0107e919298b758201c85fac12d798277
BMasinde <masindeb@live.com>
2020-10-10 20:19:24 +0200

    conditionals moved outside loops

--- R/constant_muscle_mass.R
+++ R/constant_muscle_mass.R
@@ -54,7 +54,7 @@
     endMinSpeed = vector(length = n)
   )
 
-  if (speed_control == 1) {
+  if (speed_control == 1 && protein_met == 0) {
 
     for (i in seq_len(nrow(data))) {
       # things to keep track of ################################################
@@ -101,19 +101,20 @@
         )) / constants$mce
 
         # total energy produced in the interval
-        totalEnergy <- chemPower * 360
+        #totalEnergy <- chemPower * 360
 
         # change in fat mass
-        changeMass <-
-          ifelse(protein_met == 0, (totalEnergy / constants$fed) , ((totalEnergy - (totalEnergy * protein_met)) / constants$fed))
+        # changeMass <-
+        #   ifelse(protein_met == 0, (totalEnergy / constants$fed) , ((totalEnergy - (totalEnergy * protein_met)) / constants$fed))
+        changeMass <- (chemPower * 360) / constants$fed
 
         # change in airframe mass
-        changeAirframe <-
-          ifelse(protein_met == 0, 0, ((totalEnergy * protein_met) / constants$ped))
+        # changeAirframe <-
+        #   ifelse(protein_met == 0, 0, ((totalEnergy * protein_met) / constants$ped))
         # protein has to be hydrated
 
           # update body ########################################################
-          airframeMass <- airframeMass - changeAirframe - (((totalEnergy * protein_met) / constants$ped) * constants$phr)
+        #airframeMass <- airframeMass - changeAirframe - (((totalEnergy * protein_met) / constants$ped) * constants$phr)
 
           fm <- fm - changeMass
           bm <- airframeMass + fm + muscleMass[i]
@@ -133,7 +134,161 @@
       results$endMinSpeed[i] <- minSpeed[j - 1]
     }
 
-  }else {
+  } else if (speed_control == 1 && protein_met > 0) {
+    for (i in seq_len(nrow(data))) {
+      # things to keep track of ################################################
+      bm <- allMass[i]
+      airframeMass <- allMass[i] - (fatMass[i] + muscleMass[i]) # airframe mass
+      fm <- fatMass[i]
+      dist <- 0
+      minSpeed <- vector()
+      trueSpeed <- 0
+
+      j <- 1
+      while (fm > 0.000001) {
+        # find speed and power
+        minSpeed[j] <-
+          .minpowspeed_cpp(bm = bm, ws = wingSpan[i], ipf = constants$ipf,
+                           g = constants$g, airDensity = constants$airDensity, bdc = constants$bdc)
+        #  constant speed always
+        trueSpeed <- minSpeed[1] * constants$speedRatio
+        # mechanical power #####################################################
+        mechPower <-
+          .total_Mech_Pow_cpp(
+            bm = bm,
+            ws = wingSpan[i],
+            wa = wingArea[i],
+            vt = trueSpeed,
+            g = constants$g,
+            airDensity = constants$airDensity,
+            ipf = constants$ipf,
+            bdc = constants$bdc,
+            ppc = constants$ppc
+          )
+
+
+        chemPower <- constants$vcp * (mechPower + constants$mce * .basal_metabolic_pow(
+          airframeMass,
+          # doesn't change from previous J iteration
+          muscleMass[i],
+          # changes from previous J iteration
+          taxon[i],
+          alphaPasserines,
+          alphaNonPasserines,
+          deltaPasserines,
+          deltaNonPasserines
+        )) / constants$mce
+
+        # total energy produced in the interval
+        totalEnergy <- chemPower * 360
+
+        # change in fat mass
+        changeMass <- ((totalEnergy - (totalEnergy * protein_met)) / constants$fed)
+
+        # change in airframe mass
+        changeAirframe <- (totalEnergy * protein_met) / constants$ped
+        # protein has to be hydrated
+
+        # update body ########################################################
+        airframeMass <- airframeMass - changeAirframe - (((totalEnergy * protein_met) / constants$ped) * constants$phr)
+
+        fm <- fm - changeMass
+        bm <- airframeMass + fm + muscleMass[i]
+
+        # update distance ####################################################
+        dist <- dist + trueSpeed * 360
+
+        # increment counter
+        j <- j + 1
+      }
+      # store results ##########################################################
+      results$distance[i] <- dist
+      results$allUpMass[i] <- bm
+      results$fatMass[i] <- fm
+      results$muscleMass[i] <- muscleMass[i]
+      results$startMinSpeed[i] <- minSpeed[1]
+      results$endMinSpeed[i] <- minSpeed[j - 1]
+    }
+  } else if (speed_control == 0 && protein_met == 0) {
+    for (i in seq_len(nrow(data))) {
+      # things to keep track of ################################################
+      bm <- allMass[i]
+      airframeMass <- allMass[i] - (fatMass[i] + muscleMass[i]) # airframe mass
+      fm <- fatMass[i]
+      dist <- 0
+      minSpeed <- vector()
+      trueSpeed <- 0
+
+      j <- 1
+      while (fm > 0.0001) {
+        # find speed ###########################################################
+        minSpeed[j] <-
+          .minpowspeed_cpp(bm = bm, ws = wingSpan[i], ipf = constants$ipf,
+                           g = constants$g, airDensity = constants$airDensity, bdc = constants$bdc)
+
+        # constant trueSpeed to minimum power speed always
+        trueSpeed <- minSpeed[j] * constants$speedRatio
+
+        #ratio_speed <- trueSpeed / minSpeed
+
+        # mechanical power #####################################################
+        mechPower <-
+          .total_Mech_Pow_cpp(bm = bm,
+                              ws = wingSpan[i],
+                              wa = wingArea[i],
+                              vt = trueSpeed,
+                              g = constants$g, airDensity = constants$airDensity, ipf = constants$ipf,
+                              bdc = constants$bdc, ppc = constants$ppc)
+        # chemical power including 10% heart and respiration and metabolism.
+        chemPower <- constants$vcp * (mechPower + constants$mce * .basal_metabolic_pow(
+          airframeMass,
+          # doesn't change from previous J iteration
+          muscleMass[i],
+          # changes from previous J iteration
+          taxon[i],
+          alphaPasserines,
+          alphaNonPasserines,
+          deltaPasserines,
+          deltaNonPasserines
+        )) / constants$mce
+
+        # total energy produced in the interval
+        #totalEnergy <- chemPower * 360
+
+        # change in fat mass.
+        # changeMass <-
+        #   ifelse(protein_met == 0,
+        #          (totalEnergy / constants$fed) ,
+        #          ((totalEnergy - (
+        #            totalEnergy * protein_met
+        #          )) / constants$fed))
+        changeMass <-  (chemPower * 360) / constants$fed
+
+        # change in airframe mass
+        # changeAirframe <-
+        #   ifelse(protein_met == 0, 0, ((totalEnergy * protein_met) / constants$ped))
+
+        # update body ########################################################
+        fm <- fm - changeMass
+        # airframeMass <-
+        #   airframeMass - changeAirframe - (((totalEnergy * protein_met) / constants$ped) * constants$phr)
+        bm <- airframeMass + fm + muscleMass[i]
+
+        # distance increment #################################################
+        dist <- dist + trueSpeed * 360
+
+        # increment counter
+        j <- j + 1
+      }
+      # store results ##########################################################
+      results$distance[i] <- dist
+      results$allUpMass[i] <- bm
+      results$fatMass[i] <- fm
+      results$muscleMass[i] <- muscleMass[i]
+      results$startMinSpeed[i] <- minSpeed[1]
+      results$endMinSpeed[i] <- minSpeed[j - 1]
+    }
+  }else if (speed_control == 0 && protein_met > 0) {
     for (i in seq_len(nrow(data))) {
       # things to keep track of ################################################
       bm <- allMass[i]
@@ -195,11 +350,10 @@
         fm <- fm - changeMass
         airframeMass <-
           airframeMass - changeAirframe - (((totalEnergy * protein_met) / constants$ped) * constants$phr)
-          bm <- airframeMass + fm + muscleMass[i]
+        bm <- airframeMass + fm + muscleMass[i]
 
         # distance increment #################################################
         dist <- dist + trueSpeed * 360
-
         # increment counter
         j <- j + 1
       }
@@ -211,7 +365,6 @@
       results$startMinSpeed[i] <- minSpeed[1]
       results$endMinSpeed[i] <- minSpeed[j - 1]
     }
-
   }
   return(results)
 }
