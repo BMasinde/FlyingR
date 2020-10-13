27ecc44c4344626ebc2ccf53771e76d63c97086f
BMasinde <masindeb@live.com>
2020-10-13 19:24:26 +0200

    stopover mass calculator
    with tests and documentation

--- tests/testthat/test_stopover_mass_calculator.R
+++ tests/testthat/test_stopover_mass_calculator.R
@@ -1,13 +1,21 @@
 context("stopover mass calculator")
 
-test_that("missing file and data throws error", {
-  expect_error(stopover.mass.calculator(data = NULL
+test_that("mismatch length throw error", {
+  expect_error(stopover.mass.calculator(bodyMass = c(1.1,1.2), fatMass = c(0.34),
+                                        taxon = c(1, 2), duration = 24L
   ))
 })
 
-data <- data("birds")
+
 test_that("negaative duration throws error", {
-  expect_error(stopover.mass.calculator(data = data, duration = -2
+  expect_error(stopover.mass.calculator(bodyMass = c(1.1,1.2), fatMass = c(0.34, 0.25),
+                                        taxon = c(1, 2), duration = -24L
   ))
 })
 
+
+test_that("negaative duration throws error", {
+  expect_error(stopover.mass.calculator(bodyMass = c(1.1,1.2), fatMass = c(0.34, 0.25),
+                                        taxon = c(1, 3), duration = 24L
+  ))
+})
