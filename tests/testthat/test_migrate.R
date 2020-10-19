9946278385248bc331c392e2375abc5ffe638899
BMasinde <masindeb@live.com>
2020-10-19 18:31:31 +0200

    test check zero fat mass throws error

--- tests/testthat/test_migrate.R
+++ tests/testthat/test_migrate.R
@@ -1,16 +1,27 @@
 
-context("minimum energy from protein")
+context("Migrate")
 
 data <- data("birds")
 
 test_that("out of bounds protein_met throws error", {
   expect_error(migtate(data = data, method = "cmm",
-                       speed_control = 1, protein_met = 1.2
+                       speed_control = 1, min_energy_protein = 1,2
                                     ))
 })
 
 test_that("speed control is binary", {
   expect_error(migtate(data = data, method = "cmm",
-                       speed_control = "constant_speed", protein_met = 1.2
+                       speed_control = "constant_speed", min_energy_protein = 0.05
   ))
+})
+
+birds[5, 4] <- 0
+test_that("zero fat mass throw error in migrate", {
+  expect_error(migtate(
+    data = data,
+    method = "cmm",
+    speed_control = 0,
+    min_energy_protein = 0.05
+  )
+)
 })
