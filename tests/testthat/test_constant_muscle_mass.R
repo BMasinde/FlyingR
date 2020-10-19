63651964e2a382b0fae193f85f9ec58189320a62
BMasinde <masindeb@live.com>
2020-10-19 15:17:52 +0200

    typos in arguments

--- tests/testthat/test_constant_muscle_mass.R
+++ tests/testthat/test_constant_muscle_mass.R
@@ -7,17 +7,17 @@ data <- data("birds")
 
 test_that("missing data throw error", {
   expect_error(.constant_muscle_mass(speed_control = 1,
-                                     cons = cons))
+                                     constants = cons))
 })
 
 test_that("missing constants throw error", {
-  expect_error(.constant_muscle_mass(data = data, peed_control = 1))
+  expect_error(.constant_muscle_mass(data = data, speed_control = 1))
 })
 
 
 test_that("missing speed control throw error", {
   expect_error(.constant_muscle_mass(data = data, speed_control =1,
-                                    cons = cons))
+                                    constants = cons))
 })
 
 
