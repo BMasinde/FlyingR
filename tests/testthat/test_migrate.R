5d159b61e8a19e0779c67d93946cfb26b3e5e7e6
B.K. Masinde <43029260+BMasinde@users.noreply.github.com>
2026-03-04 16:40:04 +0300

    Fixed function typos and placement of test data
    Each test with it's own copy of fresh data so that previous tests do not influence outcome of tests to follow.

--- tests/testthat/test_migrate.R
+++ tests/testthat/test_migrate.R
@@ -1,24 +1,30 @@
 
 context("Migrate")
 
-data <- data("birds")
+data("birds")
 
 test_that("out of bounds protein_met throws error", {
-  expect_error(migtate(data = data, method = "cmm",
+  expect_error(migrate(data = birds, method = "cmm",
                        speed_control = 1, min_energy_protein = 1,2
                                     ))
 })
 
 test_that("speed control is binary", {
-  expect_error(migtate(data = data, method = "cmm",
+  expect_error(migrate(data = birds_data, method = "cmm",
                        speed_control = "constant_speed", min_energy_protein = 0.05
   ))
 })
 
-birds[5, 4] <- 0
-test_that("zero fat mass throws error in migrate", {
-  expect_warning(migtate(
-    data = data,
+#birds[5, 4] <- 0
+test_that("zero fat mass throws warning in migrate", {
+  
+  # creating fresh copy of data
+  # Each test it's own copy of data
+  test_data <- birds
+  test_data[5, 4] <- 0
+  
+  expect_warning(migrate(
+    data = test_data,
     method = "cmm",
     speed_control = 0,
     min_energy_protein = 0.05
@@ -28,8 +34,8 @@ test_that("zero fat mass throws error in migrate", {
 
 birds[5, 2] <- 0
 test_that("zero body mass throws error in migrate", {
-  expect_error(migtate(
-    data = data,
+  expect_error(migrate(
+    data = birds_data,
     method = "cmm",
     speed_control = 0,
     min_energy_protein = 0.05
@@ -40,8 +46,8 @@ test_that("zero body mass throws error in migrate", {
 
 birds[5, 3] <- 0
 test_that("zero wingspan throws error in migrate", {
-  expect_error(migtate(
-    data = data,
+  expect_error(migrate(
+    data = birds_data,
     method = "cmm",
     speed_control = 0,
     min_energy_protein = 0.05
@@ -51,8 +57,8 @@ test_that("zero wingspan throws error in migrate", {
 
 birds[5, 6] <- 0
 test_that("zero wing area throws error in migrate", {
-  expect_error(migtate(
-    data = data,
+  expect_error(migrate(
+    data = birds_data,
     method = "cmm",
     speed_control = 0,
     min_energy_protein = 0.05
@@ -62,8 +68,8 @@ test_that("zero wing area throws error in migrate", {
 
 birds[5, 7] <- 0
 test_that("zero muscle mass throws error in migrate", {
-  expect_error(migtate(
-    data = data,
+  expect_error(migrate(
+    data = birds_data,
     method = "cmm",
     speed_control = 0,
     min_energy_protein = 0.05
